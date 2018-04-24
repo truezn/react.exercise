6dcca077a41ac345c53b25fdf96bb0df749e579d
Ning Zhang <truezn@gmail.com>
2018-04-24 23:51:37 +0800

    add more code 2

--- src/bundle.js
+++ src/bundle.js
@@ -82,7 +82,7 @@ var ReactDOM = __webpack_require__(10);
 ReactDOM.render(React.createElement(
   "h1",
   null,
-  "Hello World!!!"
+  "Hello World, Ning and Feng"
 ), document.getElementById("myapp"));
 
 /***/ }),
